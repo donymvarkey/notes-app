c53bd3baee2939f600b01807821f89bbf6f3a8c4
Dony M Varkey <dony.v@applab.qa>
2024-05-10 12:09:57 +0530

    Update profile and change password apis.

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -96,7 +96,62 @@ const login = async (req, res, next) => {
   }
 };
 
+const updateProfile = async (req, res, next) => {
+  try {
+    const data = await User.findByIdAndUpdate(req.user.user_id, req.body);
+
+    if (data) {
+      return returnResponse(
+        { code: 200, msg: "User registered successfully.", data: null },
+        res
+      );
+    }
+
+    return returnResponse(
+      { code: 400, msg: "Failed to update profile", data: null },
+      res
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+const changePassword = async (req, res, next) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+    const data = await User.findById(req.user.user_id);
+
+    const hashedPassword = hashPassword(currentPassword);
+    if (data?.password === hashedPassword) {
+      const newPasswordHashed = hashPassword(newPassword);
+      const resp = await User.findByIdAndUpdate(req.user.user_id, {
+        password: newPasswordHashed,
+      });
+      if (resp) {
+        return returnResponse(
+          { code: 200, msg: "Password changed", data: null },
+          res
+        );
+      }
+
+      return returnResponse(
+        { code: 400, msg: "Failed to change password", data: null },
+        res
+      );
+    }
+
+    return returnResponse(
+      { code: 400, msg: "Current password do not match", data: null },
+      res
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   register,
   login,
+  updateProfile,
+  changePassword,
 };
--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -5,5 +5,15 @@ const middlewares = require("../middlewares");
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
+router.put(
+  "/update/profile",
+  middlewares.isAuthorised,
+  AuthController.updateProfile
+);
+router.put(
+  "/update/password",
+  middlewares.isAuthorised,
+  AuthController.changePassword
+);
 
 module.exports = router;
